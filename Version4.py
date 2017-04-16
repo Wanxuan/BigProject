babd78cfb74d1ae06fe1a5d0b4d896da0a08efc7
Wanxuan <sweetxuan16@gmail.com>
2017-04-16 17:55:41 +0800

    change (3, 3) to 3, 3

--- Version4.py
+++ Version4.py
@@ -95,17 +95,17 @@ X_train, X_test, y_train, y_test = split_validation_set(train, target, 0.2)
 
 model = Sequential()
 
-model.add(Conv2D(32, (3, 3), padding='same',
-                 input_shape=(img_width, img_height, 3)))
+model.add(Conv2D(32, 3, 3, padding='same',
+                 input_shape=train.shape[1:]))
 model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
+model.add(Conv2D(32, 3, 3))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Conv2D(64, 3, 3, padding='same'))
 model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
+model.add(Conv2D(64, 3, 3))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
