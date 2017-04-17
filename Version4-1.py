2446f398d41aed1c877bcc1ed4159c1a002abc8a
Wanxuan <sweetxuan16@gmail.com>
2017-04-17 18:52:54 +0800

    print get_result(result)

--- Version4-1.py
+++ Version4-1.py
@@ -14,16 +14,16 @@ img_height = 480
 model = model_from_json(open('my_model.json').read())  
 model.load_weights('my_model_weights.h5')
 
-# def get_result(result):
-#     # 将 one_hot 编码解码
-#     resultstr = str(np.argmax(result))
-#     return resultstr
+def get_result(result):
+    # 将 one_hot 编码解码
+    resultstr = str(np.argmax(result))
+    return resultstr
 
 n_test = X_test.shape[0]
 index = random.randint(0, n_test-1)
 y_pred = model.predict(X_test[index].reshape(1, img_height, img_width, 3))
 print(X_test[index])
-print(y_pred)
+print(get_result(result))
 
 plt.title('real: %s\npred:%s'%(get_result(y_test[index]), get_result(y_pred)))
 plt.imshow(X_test[index,:,:,0])
