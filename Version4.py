cc26fea3dc482d4089a813b9d028f7c4dcc6b200
Wanxuan <sweetxuan16@gmail.com>
2017-04-17 20:54:20 +0800

    change to sklearn.model_selection

--- Version4.py
+++ Version4.py
@@ -12,7 +12,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 
 img_height = 480
 img_width = 640
