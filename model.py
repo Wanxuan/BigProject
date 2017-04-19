697322d48e2227a5c9f46d7bef83077c6db0e072
Wanxuan <sweetxuan16@gmail.com>
2017-04-19 15:40:39 +0800

    cifar10_cnn.py model and nb_epoch = 20

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ img_rows = 24
 img_cols = 32
 num_classes = 10
 batch_size = 32
-nb_epoch = 10
+nb_epoch = 20
 np.random.seed(133)
 
 pkl_file = open('dataset.pkl', 'rb')
@@ -20,19 +20,20 @@ X_train, X_test, y_train, y_test = pickle.load(pkl_file)
     
 model = Sequential()
 
-model.add(Conv2D(32, 3, 3, activation='relu', border_mode='same', input_shape=X_train.shape[1:]))
-model.add(Conv2D(32, 3, 3, activation='relu'))
+model.add(Conv2D(32, 3, 3, padding='same',
+                 input_shape=X_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32, 3, 3))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
+model.add(Dropout(0.25))
 
-model.add(Conv2D(64, 3, 3, activation='relu', border_mode='same'))
-model.add(Conv2D(64, 3, 3, activation='relu'))
+model.add(Conv2D(64, 3, 3, padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, 3, 3))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
-
-model.add(Conv2D(128, 3, 3, activation='relu', border_mode='same'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
+model.add(Dropout(0.25))
 
 model.add(Flatten())
 model.add(Dense(512))
@@ -41,7 +42,8 @@ model.add(Dropout(0.5))
 model.add(Dense(num_classes))
 model.add(Activation('softmax'))
 
-opt = keras.optimizers.Adam(lr=1e-3)
+# initiate RMSprop optimizer
+opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
 
 model.compile(optimizer=opt,
               loss='categorical_crossentropy',
@@ -52,5 +54,5 @@ model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=nb_epoch,
 
 
 json_string = model.to_json()  
-open('1_model.json','w').write(json_string)  
-model.save_weights('1_model_weights.h5')
+open('2_model.json','w').write(json_string)  
+model.save_weights('2_model_weights.h5')
