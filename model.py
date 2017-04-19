8d50c573340703010d868649bda66fb20a532415
Wanxuan <sweetxuan16@gmail.com>
2017-04-19 14:58:47 +0800

    change to Conv2D(32, 3, 3)

--- model.py
+++ model.py
@@ -18,10 +18,10 @@ X_train, X_test, y_train, y_test = pickle.load(pkl_file)
     
 model = Sequential()
 
-model.add(Conv2D(8, 2, 2, activation='relu', border_mode='valid', input_shape=X_train.shape[1:]))
+model.add(Conv2D(32, 3, 3, activation='relu', border_mode='valid', input_shape=X_train.shape[1:]))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(8, 2, 2, activation='relu'))
+model.add(Conv2D(64, 3, 3, activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
