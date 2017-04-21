215238a53e5172b3dc703e9d1967900fd1b15bf9
Wanxuan <sweetxuan16@gmail.com>
2017-04-21 21:05:50 +0800

    import ZeroPadding2D

--- vgg_model.py
+++ vgg_model.py
@@ -6,7 +6,7 @@ from keras.utils import np_utils
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
 
 batch_size = 32
 num_classes = 10
