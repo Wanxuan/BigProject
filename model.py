f74403483391a265b99b614a99e3fbfbe8b52537
Wanxuan <sweetxuan16@gmail.com>
2017-04-21 13:36:13 +0800

    add model.evaluate

--- model.py
+++ model.py
@@ -51,8 +51,10 @@ model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit_generator(train_generator, samples_per_epoch=X_train.shape[0], nb_epoch=25, 
-                    validation_data=validation_generator, nb_val_samples=X_test.shape[0])
+model.fit_generator(train_generator, samples_per_epoch=X_train.shape[0]//batch_size, 
+                    nb_epoch=25, validation_data=validation_generator, 
+                    nb_val_samples=X_test.shape[0]//batch_size)
+model.evaluate(X_test, y_test, batch_size=32, verbose=1, sample_weight=None)
 
 # json_string = model.to_json()  
 # open('gen_model.json','w').write(json_string)  
