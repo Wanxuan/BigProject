2f913df82696344bd4b5edaf3ca5de29199ff357
Wanxuan <sweetxuan16@gmail.com>
2017-04-21 19:30:49 +0800

    change to cifar10_model with nb_epoch=15

--- model.py
+++ model.py
@@ -23,14 +23,14 @@ model.add(Activation('relu'))
 model.add(Conv2D(32, 3, 3))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
+model.add(Dropout(0.25))
 
 model.add(Conv2D(64, 3, 3, border_mode='same'))
 model.add(Activation('relu'))
 model.add(Conv2D(64, 3, 3))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
+model.add(Dropout(0.25))
 
 model.add(Flatten())
 model.add(Dense(512))
@@ -49,20 +49,19 @@ train_generator = train_datagen.flow(X_train, y_train, batch_size=batch_size)
 validation_generator = test_datagen.flow(X_test, y_test, batch_size=batch_size)
 
 # initiate RMSprop optimizer
-# opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
-opt = keras.optimizers.SGD(lr=0.1, momentum=0.0, decay=0.0, nesterov=False)
+opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
 model.compile(optimizer=opt,
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 model.fit_generator(train_generator, samples_per_epoch=X_train.shape[0], 
-                    nb_epoch=10, validation_data=validation_generator, 
+                    nb_epoch=15, validation_data=validation_generator, 
                     nb_val_samples=X_test.shape[0])
 score = model.evaluate(X_test, y_test, batch_size=32, verbose=1, sample_weight=None)
 print(score)
 # json_string = model.to_json()  
 # open('gen_model.json','w').write(json_string)  
 # model.save_weights('gen_model_weights.h5')
-model.save_weights('SGD_model.h5')
-with open('SGD_model.json', 'w') as f:
+model.save_weights('cifar10_model.h5')
+with open('cifar10_model.json', 'w') as f:
     f.write(model.to_json())
