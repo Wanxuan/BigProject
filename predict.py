1f5320f40726d4cce17ab08a66ae18ad6de1beb9
Wanxuan <sweetxuan16@gmail.com>
2017-04-24 11:01:19 +0800

    load test.h5

--- predict.py
+++ predict.py
@@ -1,30 +1,39 @@
-import pickle
+import h5py
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.models import model_from_json
 
-img_rows = 24
-img_cols = 32
+img_rows = 240
+img_cols = 320
 
-pkl_file = open('dataset.pkl', 'rb')
-X_train, X_test, y_train, y_test = pickle.load(pkl_file)
+file = h5py.File('test.h5', 'r')
+X_test = file['X_test'][:]
+y_test = file['y_test'][:]
+file.close()
 
 #读取model  
-model = model_from_json(open('2_model.json').read())  
-model.load_weights('2_model_weights.h5')
+model = model_from_json(open('new_model.json').read())  
+model.load_weights('new_model.h5')
 
-def get_result(result):
-    # 将 one_hot 编码解码
-    resultstr = str(np.argmax(result))
-    return resultstr
+Y_pred = model.predict_proba(X_test, verbose=1)  # Keras预测概率Y_pred
+print(Y_pred[:3, ])  # 取前三张图片的十类预测概率看看
+score = model.evaluate(X_test, y_test, verbose=1) # 评估测试集loss损失和精度acc
+print('测试集 score(val_loss): %.4f' % score[0])  # loss损失
+print('测试集 accuracy: %.4f' % score[1]) # 精度acc
+print("耗时: %.2f seconds ..." % (time.time() - t0))
 
-n_test = X_test.shape[0]
-index = random.randint(0, n_test-1)
-y_pred = model.predict(X_test[index].reshape(1, img_rows, img_cols, 1))
-print(X_test[index])
-print(get_result(y_pred))
+# def get_result(result):
+#     # 将 one_hot 编码解码
+#     resultstr = str(np.argmax(result))
+#     return resultstr
 
-plt.title('real: %s\npred:%s'%(get_result(y_test[index]), get_result(y_pred)))
-plt.imshow(X_test[index,:,:,0])
-plt.axis('off')
+# n_test = X_test.shape[0]
+# index = random.randint(0, n_test-1)
+# y_pred = model.predict(X_test[index].reshape(1, img_rows, img_cols, 1))
+# print(X_test[index])
+# print(get_result(y_pred))
+
+# plt.title('real: %s\npred:%s'%(get_result(y_test[index]), get_result(y_pred)))
+# plt.imshow(X_test[index,:,:,0])
+# plt.axis('off')
