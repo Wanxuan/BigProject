824c0cf5eb14b76bf72f7f62ce7c289a785f33b9
Wanxuan <sweetxuan16@gmail.com>
2017-04-24 08:38:45 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -100,7 +100,7 @@ def read_and_normalize_train_data(x_train, y_train, x_test, y_test):
     print('Test shape:', X_test.shape)
     return X_train, X_test, y_train, y_test
 
-train, target, driver_id, unique_drivers = load_train(img_rows, img_cols, color_type)
+data, label, driver_id, unique_drivers = load_train(img_rows, img_cols, color_type)
 yfull_train = dict()
 unique_list_train = ['p002', 'p012', 'p014', 'p015', 'p016', 'p021', 'p022', 'p024',
                  'p026', 'p035', 'p039', 'p041', 'p042', 'p045', 'p047', 'p049',
