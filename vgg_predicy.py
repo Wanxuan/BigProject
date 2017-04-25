799ccca9f45844582a0c0823a2aff9434505497d
Wanxuan <sweetxuan16@gmail.com>
2017-04-25 18:33:41 +0800

    Update vgg_predicy.py

--- vgg_predicy.py
+++ vgg_predicy.py
@@ -4,8 +4,8 @@ from keras.layers import Dense, Dropout, Activation, Flatten, Input
 from keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
 
-input_tensor = Input(shape=(1, 480, 640, 3))
-model = VGG16(weights='imagenet', input_tensor=input_tensor)
+
+model = VGG16(weights='imagenet')
 
 img_path = 'test/img_1.jpg'
 img = image.load_img(img_path, target_size=(480, 640))
