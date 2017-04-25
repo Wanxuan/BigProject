b074ffaf50724067b99f0ba3b8dab9c455996daa
Wanxuan <sweetxuan16@gmail.com>
2017-04-25 18:42:17 +0800

    Update vgg_model.py

--- vgg_model.py
+++ vgg_model.py
@@ -104,7 +104,6 @@ print('Test Sample: ', x_test.shape, len(y_test))
 input_tensor = Input(shape=x_train.shape[1:])
 base_model = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
 x = base_model.output
-x = GlobalAveragePooling2D()(x)
 x = Flatten()(x)
 x = Dense(512, activation='relu')(x)
 x = Dropout(0.5)(x)
