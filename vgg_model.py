c5df60e0087471591265b576753ba81d0b062f88
Wanxuan <sweetxuan16@gmail.com>
2017-04-25 09:47:58 +0800

    delete , input_shape=x_train.shape[1:]

--- vgg_model.py
+++ vgg_model.py
@@ -101,7 +101,7 @@ print('Test Sample: ', len(x_test), len(y_test))
 #-----------------------------------Cifar10 End----------------------------------#
 
 input_tensor = Input(shape=x_train.shape[1:])
-base_model = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor, input_shape=x_train.shape[1:])
+base_model = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
 x = base_model.output
 x = Flatten()(x)
 x = Dense(256, activation='relu')(x)
