98b87b3eece4306ef803e3fcca0d348155faf63a
Wanxuan <sweetxuan16@gmail.com>
2017-04-25 10:23:23 +0800

    test cigar_10 again

--- model.py
+++ model.py
@@ -12,8 +12,17 @@ batch_size = 32
 num_classes = 10
 np.random.seed(133)
 
-pkl_file = open('dataset.pkl', 'rb')
-X_train, X_test, y_train, y_test = pickle.load(pkl_file)
+driver_pkl = open('driver.pkl', 'rb')
+driver_id, unique_drivers = pickle.load(driver_pkl)
+file = h5py.File('train.h5', 'r')
+x_train = file['data'][:]
+y_train = file['label'][:]
+file.close()
+
+file = h5py.File('test.h5', 'r')
+x_test = file['X_test'][:]
+y_test = file['y_test'][:]
+file.close()
     
 model = Sequential()
 
@@ -39,26 +48,34 @@ model.add(Dropout(0.5))
 model.add(Dense(num_classes))
 model.add(Activation('softmax'))
 
-train_datagen = ImageDataGenerator(
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
-test_datagen = ImageDataGenerator(rescale=1./255)
-train_generator = train_datagen.flow(X_train, y_train, batch_size=batch_size)
-validation_generator = test_datagen.flow(X_test, y_test, batch_size=batch_size)
-
-# initiate RMSprop optimizer
-opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+opt = keras.optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(optimizer=opt,
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit_generator(train_generator, samples_per_epoch=X_train.shape[0], 
-                    nb_epoch=15, validation_data=validation_generator, 
-                    nb_val_samples=X_test.shape[0])
-score = model.evaluate(X_test, y_test, batch_size=32, verbose=1, sample_weight=None)
-print(score)
+datagen = ImageDataGenerator(             
+        featurewise_center=False,  
+        samplewise_center=False,  
+        featurewise_std_normalization=False,  
+        samplewise_std_normalization=False,  
+        zca_whitening=False,  
+        rotation_range=0, 
+        width_shift_range=0.1, 
+        height_shift_range=0.1, 
+        horizontal_flip=True, 
+        vertical_flip=False)
+
+    datagen.fit(X_train)
+    
+model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size), 
+                    samples_per_epoch=x_train.shape[0], 
+                    nb_epoch=50, validation_data=(x_test, y_test), 
+                    nb_val_samples=x_test.shape[0])
+
+score = model.evaluate(x_test, y_test, verbose=1) # 评估测试集loss损失和精度acc
+print('Test score(val_loss): %.4f' % score[0])  # loss损失
+print('Test accuracy: %.4f' % score[1]) # 精度acc
+
 # json_string = model.to_json()  
 # open('gen_model.json','w').write(json_string)  
 # model.save_weights('gen_model_weights.h5')
