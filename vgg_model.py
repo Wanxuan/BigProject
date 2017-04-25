6ad0268cb27d923886b14a2d47e0938b68562612
Wanxuan <sweetxuan16@gmail.com>
2017-04-25 19:15:38 +0800

    delete datagen.fit(x_train) and modify datagen

--- vgg_model.py
+++ vgg_model.py
@@ -112,18 +112,10 @@ prediction = Dense(10, activation='softmax')(x)
 model = Model(input=base_model.input, output=prediction)
 
 datagen = ImageDataGenerator(
-    featurewise_center=False,  # set input mean to 0 over the dataset
-    samplewise_center=False,  # set each sample mean to 0
-    featurewise_std_normalization=False,  # divide inputs by std of the dataset
-    samplewise_std_normalization=False,  # divide each input by its std
-    zca_whitening=False,  # apply ZCA whitening
-    rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
-    width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-    height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-    horizontal_flip=True,  # randomly flip images
-    vertical_flip=False)  # randomly flip images
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
 
-datagen.fit(x_train)
    
 model.compile(loss='categorical_crossentropy',
               optimizer=keras.optimizers.SGD(lr=1e-4, momentum=0.9),
