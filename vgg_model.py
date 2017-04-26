7d42966e635479f8c52438982c01ee5506deb393
Wanxuan <sweetxuan16@gmail.com>
2017-04-27 07:16:12 +0800

    regularizers.l2(0.0001) and nb_epoch=20

--- vgg_model.py
+++ vgg_model.py
@@ -108,7 +108,7 @@ input_tensor = Input(shape=x_train.shape[1:])
 base_model = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
 x = base_model.output
 x = Flatten()(x)
-x = Dense(512, activation='relu', W_regularizer=regularizers.l2(0.001))(x)
+x = Dense(512, activation='relu', W_regularizer=regularizers.l2(0.0001))(x)
 x = Dropout(0.5)(x)
 prediction = Dense(10, activation='softmax')(x)
 
@@ -116,8 +116,8 @@ model = Model(input=base_model.input, output=prediction)
 
 datagen = ImageDataGenerator(
         rotation_range=40,
-        width_shift_range=0.1,
-        height_shift_range=0.1,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
         shear_range=0.2,
         zoom_range=0.2,
         horizontal_flip=True)
@@ -128,7 +128,7 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size), 
                     samples_per_epoch=x_train.shape[0], 
-                    nb_epoch=10, validation_data=(x_test, y_test), 
+                    nb_epoch=20, validation_data=(x_test, y_test), 
                     nb_val_samples=x_test.shape[0])
 
 # for layer in model.layers[:25]:
@@ -173,7 +173,7 @@ score = model.evaluate(x_test, y_test, verbose=1) # 评估测试集loss损失和
 print('Test score(val_loss): %.4f' % score[0])  # loss损失
 print('Test accuracy: %.4f' % score[1]) # 精度acc
 
-model.save_weights('new1_model.h5')
-with open('new1_model.json', 'w') as f:
+model.save_weights('new2_model.h5')
+with open('new2_model.json', 'w') as f:
         f.write(model.to_json())
           
