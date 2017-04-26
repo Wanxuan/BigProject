d1813f71e706932e7689db187113c82850540a39
Wanxuan <sweetxuan16@gmail.com>
2017-04-26 09:36:57 +0800

    change to W_regularizer
    __init__(self, output_dim, init, activation, weights, W_regularizer, b_regularizer, activity_regularizer, W_constraint, b_constraint, bias, input_dim, **kwargs)

--- vgg_model.py
+++ vgg_model.py
@@ -108,7 +108,7 @@ input_tensor = Input(shape=x_train.shape[1:])
 base_model = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
 x = base_model.output
 x = Flatten()(x)
-x = Dense(512, activation='relu', kernel_regularizer=regularizers.l2(0.01))(x)
+x = Dense(512, activation='relu', W_regularizer=regularizers.l2(0.01))(x)
 x = Dropout(0.5)(x)
 prediction = Dense(10, activation='softmax')(x)
 
