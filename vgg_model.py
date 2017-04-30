6bfa8c11358e25562b282999e36637e9ba86777e
Wanxuan <sweetxuan16@gmail.com>
2017-04-30 11:07:21 +0800

    callbacks, opt, nb_epoch=50
    callbacks=[earlyStop, tensorBoard]
    opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)

--- vgg_model.py
+++ vgg_model.py
@@ -70,40 +70,6 @@ start = time.clock()
 # print('Train drivers: ', unique_list_train)
 # print('Test drivers: ', unique_list_valid)
 
-#-----------------------------------Cifar10----------------------------------#
-# model = Sequential()
-
-# model.add(Conv2D(32, 3, 3, border_mode='same',
-#                  input_shape=x_train.shape[1:]))
-# model.add(Activation('relu'))
-# model.add(Conv2D(32, 3, 3))
-# model.add(Activation('relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.25))
-
-# model.add(Conv2D(64, 3, 3, border_mode='same'))
-# model.add(Activation('relu'))
-# model.add(Conv2D(64, 3, 3))
-# model.add(Activation('relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.25))
-
-# model.add(Conv2D(64, 3, 3, border_mode='same'))
-# model.add(Activation('relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.25))
-
-# model.add(Flatten())
-# model.add(Dense(512))
-# model.add(Activation('relu'))
-# model.add(Dropout(0.5))
-# model.add(Dense(num_classes))
-# model.add(Activation('softmax'))
-
-# opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
-# model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-#-----------------------------------Cifar10 End----------------------------------#
-
 input_tensor = Input(shape=x_train.shape[1:])
 base_model = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
 x = base_model.output
@@ -123,46 +89,14 @@ datagen = ImageDataGenerator(
         horizontal_flip=True)
 
    
-model.compile(loss='categorical_crossentropy',
-              optimizer=keras.optimizers.SGD(lr=1e-4, momentum=0.9),
-              metrics=['accuracy'])
+opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+earlyStop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, verbose=0, mode='auto')
+tensorBoard = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0)
+model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size), 
-                    samples_per_epoch=x_train.shape[0], 
-                    nb_epoch=20, validation_data=(x_test, y_test), 
+                    samples_per_epoch=x_train.shape[0], callbacks=[earlyStop, tensorBoard],
+                    nb_epoch=50, validation_data=(x_test, y_test), 
                     nb_val_samples=x_test.shape[0])
-
-# for layer in model.layers[:25]:
-#     layer.trainable = False
-
-# base_model.fit(x_train, y_train, batch_size=batch_size, nb_epoch=1, verbose=1, 
-#                validation_data=(x_test, y_test))
-
-# datagen = ImageDataGenerator(
-#         shear_range=0.2,
-#         zoom_range=0.2,
-#         horizontal_flip=True)
-
-# train_data = base_model.predict_generator(
-#     datagen.flow(x_train, y_train, batch_size=32), len(x_train))
-# test_data = base_model.predict_generator(
-#     datagen.flow(x_test, y_test, batch_size=32), len(x_test))
-
-# model = Sequential()
-# model.add(Flatten(input_shape=train_data.shape[1:]))
-# model.add(Dense(256, activation='relu'))
-# model.add(Dropout(0.5))
-# model.add(Dense(10, activation='softmax'))
-
-# model.compile(optimizer='rmsprop',
-#               loss='categorical_crossentropy',
-#               metrics=['accuracy'])
-
-# model.fit(x_train, y_train, nb_epoch=10, 
-#           batch_size=32, verbose=1,
-#           validation_split=0.2,
-#           validation_data=(x_test, y_test))
-
-#-----------------------------VGG---------------------------------#
 
 # for i, layer in enumerate(base_model.layers):
 #     print(i, layer.name)
@@ -173,7 +107,7 @@ score = model.evaluate(x_test, y_test, verbose=1) # 评估测试集loss损失和
 print('Test score(val_loss): %.4f' % score[0])  # loss损失
 print('Test accuracy: %.4f' % score[1]) # 精度acc
 
-model.save_weights('new2_model.h5')
-with open('new2_model.json', 'w') as f:
+model.save_weights('new3_model.h5')
+with open('new3_model.json', 'w') as f:
         f.write(model.to_json())
           
