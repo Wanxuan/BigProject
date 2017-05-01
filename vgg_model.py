5b157692094ece3dceed4f063b44478f3d8bd007
Wanxuan <sweetxuan16@gmail.com>
2017-05-01 08:01:28 +0800

    delete early stop

--- vgg_model.py
+++ vgg_model.py
@@ -90,11 +90,11 @@ datagen = ImageDataGenerator(
 
    
 opt = keras.optimizers.SGD(lr=1e-4, momentum=0.9)
-earlyStop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='auto')
+# earlyStop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='auto')
 
 model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size), 
-                    samples_per_epoch=x_train.shape[0], callbacks=[earlyStop],
+                    samples_per_epoch=x_train.shape[0],
                     nb_epoch=50, validation_data=(x_test, y_test), 
                     nb_val_samples=x_test.shape[0])
 
