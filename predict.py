93139e9653771d04c89ebc1c395b3bae84a9dd4a
Wanxuan <sweetxuan16@gmail.com>
2017-05-03 14:58:22 +0800

    define a create_submission function

--- predict.py
+++ predict.py
@@ -1,40 +1,38 @@
 import h5py, random
 import keras
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from keras.models import model_from_json
+from sklearn.metrics import log_loss
 
 img_rows = 240
 img_cols = 320
+num_epochs = 50
 
-file = h5py.File('test.h5', 'r')
-X_test = file['X_test'][:]
-y_test = file['y_test'][:]
+file = h5py.File('final_test.h5', 'r')
+x_test = file['x_test'][:]
 file.close()
 
 #读取model  
-model = model_from_json(open('e20_model.json').read())  
-model.load_weights('e20_model.h5')
+model = model_from_json(open('new3_model.json').read())  
+model.load_weights('new3_model.h5')
 
-opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
-model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+def create_submission(predictions, test_id, info):
+  result = pd.DataFrame(predictions, columns=['c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9'])
+  result.loc[:, 'img'] = pd.Series(test_id, index=result.index)
+  now = datetime.datetime.now()
+  if not os.path.isdir('submit'):
+    os.mkdir('submit')
+  suffix = info + '_' + str(now.strftime("%Y-%m-%d-%H-%M"))
+  sub_file = os.path.join('submit', 'submission_' + suffix + '.csv')
+  result.to_csv(sub_file, index=False)
 
-score = model.evaluate(X_test, y_test, verbose=1) # 评估测试集loss损失和精度acc
-print('Test score(val_loss): %.4f' % score[0])  # loss损失
-print('Test accuracy: %.4f' % score[1]) # 精度acc
-
-
-# def get_result(result):
-#     # 将 one_hot 编码解码
-#     resultstr = str(np.argmax(result))
-#     return resultstr
-
-# n_test = X_test.shape[0]
-# index = random.randint(0, n_test-1)
-# y_pred = model.predict(X_test[index].reshape(1, img_rows, img_cols, 1))
-# print(X_test[index])
-# print(get_result(y_pred))
-
-# plt.title('real: %s\npred:%s'%(get_result(y_test[index]), get_result(y_pred)))
-# plt.imshow(X_test[index,:,:,0])
-# plt.axis('off')
+score = log_loss(y_test, y_pred)
+print('Score log_loss:', score)
+y_pred = model.predict(x_test, batch_size=128, verbose=1)
+print(y_pred.shape)
+test_pred = []
+test_pred.append(y_pred)
+info_string = 'loss_' + str(score) + '_ep_' + str(num_epochs)
+create_submission(test_pred, test_id, info_string)
