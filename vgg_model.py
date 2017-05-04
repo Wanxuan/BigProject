865658b08b8c43def464e58548811864875490b3
Wanxuan <sweetxuan16@gmail.com>
2017-05-04 23:48:59 +0800

    Update vgg_model.py

--- vgg_model.py
+++ vgg_model.py
@@ -70,8 +70,7 @@ start = time.clock()
 print('Train drivers: ', unique_list_train)
 print('Test drivers: ', unique_list_valid)
 
-input_t = Input(shape=x_train.shape[1:])
-input_tensor= Dropout(0.5, input_shape=(x_train.shape[0],))(input_t)
+input_tensor = Input(shape=x_train.shape[1:])
 base_model = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
 x = base_model.output
 x = Flatten()(x)
