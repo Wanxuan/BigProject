dc3bc77afb0451f3a4936afeb29f39e17ea0666b
Wanxuan <sweetxuan16@gmail.com>
2017-05-04 22:51:24 +0800

    kernel_regularizer

--- vgg_model.py
+++ vgg_model.py
@@ -33,7 +33,7 @@ file = h5py.File('test.h5', 'r')
 x_test = file['X_test'][:]
 y_test = file['y_test'][:]
 file.close()
-
+print("Data loaded!")
 # def split_validation_set(train, target, test_size):
 # #     random_state = 51
 #         X_train, X_test, y_train, y_test = train_test_split(train, target, test_size=test_size)
@@ -74,7 +74,7 @@ input_tensor = Input(shape=x_train.shape[1:])
 base_model = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
 x = base_model.output
 x = Flatten()(x)
-x = Dense(512, activation='relu', W_regularizer=regularizers.l2(0.0001), activity_regularizer=regularizers.l1(0.01))(x)
+x = Dense(512, activation='relu', kernel_regularizer=regularizers.l2(0.0001), activity_regularizer=regularizers.l1(0.01))(x)
 x = Dropout(0.5)(x)
 prediction = Dense(10, activation='softmax')(x)
 
