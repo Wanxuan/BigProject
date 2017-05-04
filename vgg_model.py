2d8f2145f9b4193608be9b9add8fbdddfb4dbb68
Wanxuan <sweetxuan16@gmail.com>
2017-05-04 23:15:47 +0800

    activity_l2

--- vgg_model.py
+++ vgg_model.py
@@ -70,14 +70,11 @@ start = time.clock()
 print('Train drivers: ', unique_list_train)
 print('Test drivers: ', unique_list_valid)
 
-WeightRegularizer.set_param(W_regularizer)
-WeightRegularizer.set_param(activity_regularizer)
-
 input_tensor = Input(shape=x_train.shape[1:])
 base_model = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
 x = base_model.output
 x = Flatten()(x)
-x = Dense(512, activation='relu', W_regularizer=regularizers.l2(0.0001), activity_regularizer=regularizers.l1(0.01))(x)
+x = Dense(512, activation='relu', W_regularizer=regularizers.l2(0.0001), activity_regularizer=regularizers.activity_l2(0.01))(x)
 x = Dropout(0.5)(x)
 prediction = Dense(10, activation='softmax')(x)
 
