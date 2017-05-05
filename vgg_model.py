0301be467fb4e58b4baaaa8ee1381118c88a4b46
Wanxuan <sweetxuan16@gmail.com>
2017-05-05 21:59:31 +0800

    TypeError: InceptionV3()
    got an unexpected keyword argument 'pooling'

--- vgg_model.py
+++ vgg_model.py
@@ -71,7 +71,7 @@ print('Train drivers: ', unique_list_train)
 print('Test drivers: ', unique_list_valid)
 
 input_tensor = Input(shape=x_train.shape[1:])
-base_model = InceptionV3(include_top=False, weights='imagenet', input_tensor=input_tensor, pooling='avg')
+base_model = InceptionV3(include_top=False, weights='imagenet', input_tensor=input_tensor)
 x = base_model.output
 # x = Dropout(0.5)(x)
 x = Flatten()(x)
