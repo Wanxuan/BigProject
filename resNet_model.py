0c0564ee9e5e2bfd13d58f45ec9d3c2ce756bbcb
Wanxuan <sweetxuan16@gmail.com>
2017-05-06 23:31:48 +0800

    from keras.models import model_from_json

--- resNet_model.py
+++ resNet_model.py
@@ -8,6 +8,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers, regularizers
 from sklearn.model_selection import train_test_split
 from keras.applications.resnet50 import ResNet50
+from keras.models import model_from_json
 import numpy as np
 import pickle, h5py, time
 
